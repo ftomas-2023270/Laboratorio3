adcd024dcf7de79b6ec409830e0b2b4f400d17d8
LC2020429 <lcordova-2020429@kinal.edu.gt>
2024-08-23 15:39:32 -0600

    build:configuracion del modelo televisor

--- src/org/laboratorio3/model/Computadora.java
+++ src/org/laboratorio3/model/Computadora.java
@@ -4,6 +4,25 @@ package org.laboratorio3.model;
  *
  * @author informatica
  */
-public class Computadora {
+public class Computadora extends Dispositivo {
+    private String sistemaOperativo;
+
+    public Computadora() {
+    }
+
+    public Computadora(String sistemaOperativo, String marca, String modelo, int anio) {
+        super(marca, modelo, anio);
+        this.sistemaOperativo = sistemaOperativo;
+    }
+
+    public String getSistemaOperativo() {
+        return sistemaOperativo;
+    }
+
+    public void setSistemaOperativo(String sistemaOperativo) {
+        this.sistemaOperativo = sistemaOperativo;
+    }
     
+    
+   
 }
--- src/org/laboratorio3/model/Televisor.java
+++ src/org/laboratorio3/model/Televisor.java
@@ -4,6 +4,27 @@ package org.laboratorio3.model;
  *
  * @author informatica
  */
-public class Televisor {
+public class Televisor extends Dispositivo{
+    private String tamanoPantalla;
+    
+    public Televisor(){
+        
+    }
+
+    public Televisor(String tamanoPantalla, String marca, String modelo, int anio) {
+        super(marca, modelo, anio);
+        this.tamanoPantalla = tamanoPantalla;
+    }
+
+    public String getTamanoPantalla() {
+        return tamanoPantalla;
+    }
+
+    public void setTamanoPantalla(String tamanoPantalla) {
+        this.tamanoPantalla = tamanoPantalla;
+    }
+    
+    
+    
     
 }
