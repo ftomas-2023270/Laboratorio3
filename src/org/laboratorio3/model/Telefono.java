312c19d07452f55a90e8955106621311a7bbac93
LC2020429 <lcordova-2020429@kinal.edu.gt>
2024-08-23 15:33:44 -0600

    build:configuracion del modelo telefono

--- src/org/laboratorio3/model/Telefono.java
+++ src/org/laboratorio3/model/Telefono.java
@@ -5,5 +5,23 @@ package org.laboratorio3.model;
  * @author informatica
  */
 public class Telefono {
+    private String tipo;
+
+    public Telefono() {
+    }
+
+    public Telefono(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
     
 }
