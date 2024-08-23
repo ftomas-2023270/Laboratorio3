6f4124643c7fe0790002705d8ec4261bc63d18d9
Erick Socop <esocop-2023011@kinal.edu.gt>
2024-08-23 14:54:10 -0600

    add:se creo los atributos, gets, sets, el metodo abstracto de empleado y metodo normal de trabajar

--- src/org/laboratorio3/model/Empleado.java
+++ src/org/laboratorio3/model/Empleado.java
@@ -1,6 +1,47 @@
 package org.laboratorio3.model;
 
 
-public class Empleado {
+abstract class Empleado {
+    private String nombre;
+    private int edad;
+    private int salario;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, int edad, int salario) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
     
+    abstract void trabajar();
+    
+    public int trabajar(int horas){
+        return horas;
+    }
 }
